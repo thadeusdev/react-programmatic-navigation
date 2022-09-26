23cc95a84fa07cd0d6d8b0a66603969a4719ead1
thadeusdev <109853680+thadeusdev@users.noreply.github.com>
2022-09-26 17:54:10 +0300

    Add files via upload

---
+++ src/components/App.js
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import { Switch, Route } from "react-router-dom";
+import NavBar from "./NavBar";
+import Home from "./Home";
+import About from "./About";
+import ProjectList from "./ProjectList";
+import ProjectDetail from "./ProjectDetail";
+import NewProject from "./NewProject";
+
+function App() {
+    
+    return (
+        <div>
+            <NavBar />
+            <Switch>
+                <Route path="/about">
+                    <About />
+                </Route>
+                <Route exact path="/projects">
+                    <ProjectList />
+                </Route>
+                <Route path="/projects/new">
+                    <NewProject />
+                </Route>
+                <Route path="/projects/:id">
+                    <ProjectDetail />
+                </Route>
+                <Route exact path="/">
+                    <Home />
+                </Route>
+                <Route path="*">
+                    <h1>404 not found</h1>
+                </Route>
+            </Switch>
+        </div>
+    );
+}
+
+export default App;
+
